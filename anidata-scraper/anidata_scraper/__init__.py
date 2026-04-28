a9cbed175b906e44ece4676de0c387885e495196
AnjaraAndriatseheno <nanjaraandria@gmail.com>
2026-04-28 10:57:37 +0200

    fix: update __init__.py imports

--- anidata-scraper/anidata_scraper/__init__.py
+++ anidata-scraper/anidata_scraper/__init__.py
@@ -1,10 +1,5 @@
 """AniData Lab scraper package."""
-from anidata_scraper.scraper import (
-    AniDexScraper,
-    Anime,
-    NewsArticle,
-    scrape_to_file,
-)
+from anidata_scraper.scraper import fetch, parse, save, scrape_all
 
-__all__ = ["AniDexScraper", "Anime", "NewsArticle", "scrape_to_file"]
+__all__ = ["fetch", "parse", "save", "scrape_all"]
 __version__ = "1.0.0"
